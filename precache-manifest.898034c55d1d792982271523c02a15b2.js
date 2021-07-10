cb19ffb0933e25cf82897248855eac59b1693962
Ubuntu <cheekeat_lim1991@hotmail.com>
2021-07-10 16:28:56 +0800

    Updates

---
+++ precache-manifest.898034c55d1d792982271523c02a15b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fedba659907ac5fb354",
+    "url": "/YieldFarm_BridgeEthBsc/static/css/main.4633c5f6.chunk.css"
+  },
+  {
+    "revision": "2fedba659907ac5fb354",
+    "url": "/YieldFarm_BridgeEthBsc/static/js/main.2fedba65.chunk.js"
+  },
+  {
+    "revision": "c7b33c0684bd17c93fe1",
+    "url": "/YieldFarm_BridgeEthBsc/static/css/1.e1aff841.chunk.css"
+  },
+  {
+    "revision": "c7b33c0684bd17c93fe1",
+    "url": "/YieldFarm_BridgeEthBsc/static/js/1.c7b33c06.chunk.js"
+  },
+  {
+    "revision": "07e6d9e3ab9ce0f67ee3",
+    "url": "/YieldFarm_BridgeEthBsc/static/js/runtime~main.07e6d9e3.js"
+  },
+  {
+    "revision": "0c120fa39e43d50028f568b05c1178b2",
+    "url": "/YieldFarm_BridgeEthBsc/static/media/pteria.0c120fa3.png"
+  },
+  {
+    "revision": "57f6e0370c3738697f35c2de7fe29461",
+    "url": "/YieldFarm_BridgeEthBsc/index.html"
+  }
+];
