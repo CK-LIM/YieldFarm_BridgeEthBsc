3d035613912e666b662ba12ab8dd7f5097de9a4f
CK-LIM <80962037+CK-LIM@users.noreply.github.com>
2021-07-28 17:50:04 +0800

    Update unstake-token.js

--- scripts/unstake-token.js
+++ scripts/unstake-token.js
@@ -3,11 +3,7 @@ const XToken = artifacts.require('XToken')
 const LPXToken = artifacts.require('LPXToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
-const Prikeys = ['8e6be4d7056eb2a4eba24a407f6ddd735e408187f8596ef5c3be00efc7a35935',
-    'fee5d041188b2e4020492e007886ec694e3fb9ba1ba005311cb298ea55e06232',
-    'b397c127f27744f90ffd55f9a9a08eb25e7bb211838b046d6893f55f3d59ec2f',
-    '5f00a17192e03ec0fb8237d21c3dfc69aaf1ef6fbd7180091c1f8a4b9443bf0f',
-    '80f2aa872eebb4588bef54dfc315295e484226ec144b45a4fe76999bf8f140f9']
+const Prikeys = ['private key']
 
 let accounts = [];
 let index = 5
